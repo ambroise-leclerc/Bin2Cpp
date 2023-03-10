cbbd1b20345b5198d2cfd465f4677ffd4d3b2147
Ambroise Leclerc <github.com@physigenics.com>
2023-03-10 15:50:52 +0100

    Code coverage support

--- bin2cpp.cpp
+++ bin2cpp.cpp
@@ -3,11 +3,11 @@
 /// @brief Command-line utility which converts a binary file to a C++ source code file
 /// 
 /// This file is part of Bin2Cpp.
-/// Bin2Cpp is free software : you can redistribute it and or modify it under the terms of the GNU General Public License as published
-/// by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// Bin2Cpp is free software : you can redistribute it and or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation, either version 3 of the License, or any later version.
 /// Bin2Cpp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-/// You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+/// You should have received a copy of the GNU General Public License along with Bin2Cpp. If not, see <https://www.gnu.org/licenses/>.
 #include "SourceCode.hpp"
 
 #include <cxxopts.hpp>
