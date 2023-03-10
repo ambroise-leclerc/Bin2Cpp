4961ba5dc3cd257ae5bd0cb9101141ff1531c3fa
Ambroise Leclerc <ambroise.leclerc@webfront.fr>
2023-03-10 15:23:04 +0100

    preamble

--- SourceCode.hpp
+++ SourceCode.hpp
@@ -4,11 +4,10 @@
 ///
 /// This file is part of Bin2Cpp.
 /// Bin2Cpp is free software : you can redistribute it and or modify it under the terms of the GNU General Public License as
-/// published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. Bin2Cpp
-/// is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// published by the Free Software Foundation, either version 3 of the License, or any later version.
+/// Bin2Cpp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-/// You should have received a copy of the GNU General Public License along with Foobar. If not, see
-/// <https://www.gnu.org/licenses/>.
+/// You should have received a copy of the GNU General Public License along with Bin2Cpp. If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
 #include <array>
@@ -19,8 +18,6 @@
 #include <random>
 #include <sstream>
 #include <utility>
-#include <vector>
-#include <version>
 
 #if __has_include(<fmt/core.h>)
 #include <fmt/core.h>
