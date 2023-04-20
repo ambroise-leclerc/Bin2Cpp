1a4a70e537d94854925992ae2fdc34f56c11f878
Ambroise Leclerc <github.com@physigenics.com>
2023-04-20 15:50:48 +0200

    Add gzip and brotli file format detection for encoding information (#10)

--- SourceCode.hpp
+++ SourceCode.hpp
@@ -7,11 +7,13 @@
 /// published by the Free Software Foundation, either version 3 of the License, or any later version.
 /// Bin2Cpp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-/// You should have received a copy of the GNU General Public License along with Bin2Cpp. If not, see <https://www.gnu.org/licenses/>.
+/// You should have received a copy of the GNU General Public License along with Bin2Cpp. If not, see
+/// <https://www.gnu.org/licenses/>.
 #pragma once
 
 #include <array>
 #include <bit>
+#include <cstddef>
 #include <cstring>
 #include <filesystem>
 #include <fstream>
@@ -28,6 +30,33 @@ using fmt::format;
 #include <format>
 #endif
 
+enum class DataFormat { raw, gzip, brotli };
+DataFormat detectFormat(std::filesystem::path input) {
+    // Detection from extension
+    DataFormat dataFormat{DataFormat::raw};
+    if (input.extension() == ".gz")
+        dataFormat = DataFormat::gzip;
+    else if (input.extension() == ".br")
+        dataFormat = DataFormat::brotli;
+
+    // Verification
+    if (dataFormat == DataFormat::gzip) {
+        std::ifstream testFile;
+        std::array<std::byte, 2> buffer;
+        const std::byte gzipId1{0x1f}, gzipId2{0x8b};
+
+        testFile.open(input, std::ios::binary);
+        testFile.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
+        testFile.close();
+        if ((buffer[0] != gzipId1) || (buffer[1] != gzipId2)) {
+            std::clog << "File has a gzip extension but not a gzip preamble : type is identified as 'raw'\n";
+            dataFormat = DataFormat::raw;
+        }
+    }
+
+    return dataFormat;
+}
+
 template<typename InputStream, typename OutputStream>
 class SourceCode {
     std::string structName, valueType;
@@ -37,22 +66,24 @@ class SourceCode {
 
     bool optionalDecoder{false};
 
+    DataFormat dataFormat{DataFormat::raw};
+
 public:
     SourceCode(size_t maxColumns, size_t outputBitwidth, std::string structName)
         : structName(std::move(structName)), maxColumns(maxColumns) {
         setOutputBitwidth(outputBitwidth);
     }
 
-    SourceCode(std::filesystem::path input, std::filesystem::path output, size_t maxColumns, size_t outputBitwidth)
-        : SourceCode(maxColumns, outputBitwidth, output.stem().string()) {
+    SourceCode(std::filesystem::path input, size_t maxColumns, size_t outputBitwidth, std::string structName)
+        : SourceCode(maxColumns, outputBitwidth, structName) {
+        dataFormat = detectFormat(input);
         src.open(input, std::ios::binary);
-        dst.open(output);
-        std::cout << "Code will be generated in file : " << std::filesystem::absolute(output) << "\n";
     }
 
-    SourceCode(std::filesystem::path input, size_t maxColumns, size_t outputBitwidth, std::string structName)
-        : SourceCode(maxColumns, outputBitwidth, std::move(structName)) {
-        src.open(input, std::ios::binary);
+    SourceCode(std::filesystem::path input, std::filesystem::path output, size_t maxColumns, size_t outputBitwidth)
+        : SourceCode(input, maxColumns, outputBitwidth, output.stem().string()) {
+        dst.open(output);
+        std::clog << "Code will be generated in file : " << std::filesystem::absolute(output) << "\n";
     }
 
     void setOptionalDecoder(bool set = true) { optionalDecoder = set; }
@@ -68,8 +99,7 @@ public:
 
         dst << "#pragma once\n\n";
         dst << "#include <array>\n";
-        if (optionalDecoder)
-            dst << "#include <bit>\n#include <cstring>\n";
+        if (optionalDecoder) dst << "#include <bit>\n#include <cstring>\n";
         dst << "\nstruct " << structName << " {\n";
 
         auto fileSize = src.tellg();
@@ -77,6 +107,7 @@ public:
         fileSize = src.tellg() - fileSize;
         src.clear();
         src.seekg(0);
+        dst << getEncodingString(dataFormat);
         dst << "    static constexpr size_t data_size{ " << fileSize << " };\n";
         dst << "    static constexpr std::array<" << valueType << ", " << 1 + fileSize / bytesPerValue << "> data {\n    ";
         dst << std::hex << std::setfill('0');
@@ -153,6 +184,15 @@ private:
     int testsCount{0}, failedTestsCount{0};
 
 private:
+    constexpr std::string_view getEncodingString(DataFormat format) {
+        switch (format) {
+            case DataFormat::raw: return {};
+            case DataFormat::gzip: return "    static constexpr std::string_view encoding {\"gzip\"};\n";
+            case DataFormat::brotli: return "    static constexpr std::string_view encoding {\"br\"};\n";
+        }
+        return {};
+    }
+
     void setOutputBitwidth(size_t bits) {
         if (bits != 16 and bits != 32 and bits != 64) bits = 8;
         valueType = "uint" + std::to_string(bits) + "_t";
@@ -197,7 +237,8 @@ private:
     static istream& read(char* s, size_t count) { return get(s, count); }\n\
     static istream& get(char* s, size_t count) {\n\
         static istream stream{.readIndex = 0};\n\
-        static std::array<char, " + width +"> chunk;\n\
+        static std::array<char, " +
+               width + "> chunk;\n\
 \n\
         stream.lastReadCount = 0;\n\
         if (stream.eof()) return stream;\n\
@@ -207,8 +248,10 @@ private:
                 stream.lastReadCount = index;\n\
                 return stream;\n\
             }\n\
-            if (stream.readIndex % " + width + " == 0) memcpy(chunk.data(), &data[stream.readIndex / " + width + "], " + width + ");\n\
-            size_t readIndex = std::endian::native == std::endian::big ? stream.readIndex % " + width + " : " + width + " - 1 - (stream.readIndex % " + width + ");\n\
+            if (stream.readIndex % " +
+               width + " == 0) memcpy(chunk.data(), &data[stream.readIndex / " + width + "], " + width + ");\n\
+            size_t readIndex = std::endian::native == std::endian::big ? stream.readIndex % " +
+               width + " : " + width + " - 1 - (stream.readIndex % " + width + ");\n\
             s[index] = chunk[readIndex];\n\
             \n\
             stream.readIndex++;\n\
@@ -217,5 +260,5 @@ private:
         stream.lastReadCount = count;\n\
         return stream;\n\
     }";
-}
+    }
 };
